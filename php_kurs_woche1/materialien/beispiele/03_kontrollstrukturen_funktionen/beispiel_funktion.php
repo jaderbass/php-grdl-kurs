c9458256f83b6327e40a6bd1db658ab6174b66a6
jaderbass <joerg@jaderbass.de>
2025-11-12 13:11:33 +0000

    Funktionen-Beispiele angelegt

--- php_kurs_woche1/materialien/beispiele/03_kontrollstrukturen_funktionen/beispiel_funktion.php
+++ php_kurs_woche1/materialien/beispiele/03_kontrollstrukturen_funktionen/beispiel_funktion.php
@@ -1,5 +1,64 @@
 <?php
 declare(strict_types=1);
+error_reporting(E_ALL);
+ini_set('display_errors', true);
+
+// Funktionsdefinition
+function hallo($name) {
+  // echo-Ausgaben in Funktionionen möglichst vermeiden
+  echo "<p>Hallo $name aus der Funktion!</p>";
+}
+
+// Funktion mit Rückgabe
+function summe($a, $b) {
+  $erg = $a + $b;
+  return $erg;
+
+  echo 'Quatsch';
+}
+
+// fixe Datentypen
+function brutto(float $netto, float $mwst = 0.19):float {
+  return $netto * (1 + $mwst);
+}
+
+// variable Parameter
+function ausgabe( string $name, int $alter ) :string {
+  // Werte der übergebenen Parameter
+  $params = func_get_args();
+
+  echo '<pre>', var_dump($params), '</pre>';
+
+  // Anzahl der übergebenen Parameter
+  $num_params = func_num_args();
+
+  echo "<p>Dieser Funktion wurden $num_params Parameter übergeben.</p>";
+
+  $sex = func_get_arg(2);
+
+  return "$name ist $alter Jahre alt und ist $sex.";
+}
+
+// variadische Parameter
+// nutzen ein Array als Parameter-Definition
+function zeigeZutaten( ...$args ) {
+  $ret = '<ul>';
+
+  foreach($args as $arg) {
+    $ret .= "<li>$arg</li>";
+  }
+
+  $ret .= '</ul>';
+
+  return $ret;
+}
+
+
+function personInfo(string $name, string $vorname, int $alter) {
+  return "$vorname $name ist $alter Jahre alt.";
+}
+
+$person = ['Müller', 'Peter', 39];
 
 ?>
 <!doctype html>
@@ -13,7 +72,18 @@ declare(strict_types=1);
 <body>
   <header><h1>Funktionen</h1></header>
   <main class="container">
-    <p>Netto: 100.00 € → Brutto:  €</p>
+    <?php hallo('Jörg'); ?>
+    <?php hallo('Kathrin'); ?>
+    <?php $sum = summe(37, 5); ?>
+
+    <p><?= ausgabe('Martin', 23, 'männlich', 'Motorrad', 4, false); ?></p>
+    <p><?= zeigeZutaten('Butter', 'Mehl', 'Eier', 'Safran', 'Schmalz', 'Salz'); ?></p>
+
+    <p><?= personInfo( ...$person ); ?></p>
+
+    <p>Das utlimative Ergebnis ist: <?= $sum ?>.</p>
+
+    <p>Netto: 100.00 € → Brutto: <?= brutto(100.5); ?> €</p>
   </main>
 </body>
 </html>
