e757f513a17e818835598294bc9cf587ade73859
jaderbass <joerg@jaderbass.de>
2025-11-17 13:35:52 +0000

    Beispiel: erste Objekte anlegen

--- php_kurs_woche2/materialien/beispiele_notizmanager/04_oop/class/Note.php
+++ php_kurs_woche2/materialien/beispiele_notizmanager/04_oop/class/Note.php
@@ -1,3 +1,25 @@
 <?php
 declare(strict_types=1);
+class Note {
+  /* Öffentliche Klassen sollten vermieden werden
+  public string $title;
+  public string $content;
 
+  public function __construct(string $title, string $content) {
+    $this->title = $title;
+    $this->content = $content;
+  } */
+
+  /* Constructor Property Promotion */
+  public function __construct(private string $title, private string $content) {
+    // 
+  }
+
+  public function getTitle() {
+    return $this->title;
+  }
+
+  public function getContent() {
+    return $this->content;
+  }
+}
