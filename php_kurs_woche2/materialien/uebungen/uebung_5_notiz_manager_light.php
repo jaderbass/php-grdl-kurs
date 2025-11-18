026dd83869cca65ae2d0fbb9d27c75369d448b40
jaderbass <joerg@jaderbass.de>
2025-11-18 13:49:45 +0000

    Aufgabenstellung Übung 5 angepasst

--- php_kurs_woche2/materialien/uebungen/uebung_5_notiz_manager_light.php
+++ php_kurs_woche2/materialien/uebungen/uebung_5_notiz_manager_light.php
@@ -2,10 +2,11 @@
 declare(strict_types=1);
 /**
  * Aufgabe:
+ * 0) Einer von Euch legt ein öffentliches Git-Repo an von wo aus Ihr alle pushen und pullen könnt (optional aber hilfreich)
  * 1) Erstelle die Struktur: data/, inc/, class/, public/
  * 2) Lade Notizen aus data/notes.json und zeige sie in public/index.php.
  * 3) Implementiere add.php und delete.php (POST) Hinweis: verstecktes Formularfeld.
- * 4) index.php:
+ * 4) index.php (Grundgerüst wie hier in dieser Datei):
  *    - Ausgabe der Notizen (wenn leeres Array: Info ausgeben)
  *    - Formular zum Hinzufügen neuer Notizen
  *    - Formular zum Löschen der entsprechenden Notiz (<input type="hidden">)
