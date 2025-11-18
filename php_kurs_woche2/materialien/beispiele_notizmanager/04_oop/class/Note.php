b8eb1fb12be98e3ae64c8c262d942a1e996401d5
jaderbass <joerg@jaderbass.de>
2025-11-18 10:02:46 +0000

    OOP: magische Methoden, Destruktor, Readonly, Static, KOnstanten, Vererbung, interfaces, enum, serialize, unserialize

--- php_kurs_woche2/materialien/beispiele_notizmanager/04_oop/class/Note.php
+++ php_kurs_woche2/materialien/beispiele_notizmanager/04_oop/class/Note.php
@@ -10,9 +10,25 @@ class Note {
     $this->content = $content;
   } */
 
+  /**
+   * Konstanten sind immer public
+   * sie können keine andere Sichtbarbkeit annehmen
+   * sie sind unabhängig von einzelnen Objekten
+   * sie sind insgesamt nur einmal vorhanden
+   */
+  const TYP = 'Notiz';
+
+  /**
+   * statische Eigenschaften
+   * sind unabhängig von Objekten
+   * sind insgesamt nur einmal vorhanden
+   * sollten unmittelbar zu Beginn initialisiert werden
+   */
+  static $anzahl = 0;
+
   /* Constructor Property Promotion */
-  public function __construct(private string $title, private string $content) {
-    // 
+  public function __construct(private readonly int $number, private string $title, private string $content) {
+    self::$anzahl++;
   }
 
   public function getTitle() {
@@ -21,5 +37,29 @@ class Note {
 
   public function getContent() {
     return $this->content;
+  }
+
+  public function getNumber() {
+    return $this->number;
+  }
+
+  static function makeCopy(Note $original, int $number, string $title, string $content) {
+    $new = new Note($number, $title, $content);
+    $new->title = 'Kopie von ' . $original->title;
+    return $new;
+  }
+
+  function __toString() {
+    return "__toString() -> $this->number von " . self::$anzahl . " \"$this->title: $this->content\" vom Typ " . self::TYP;
+  }
+
+  // magische Methode zum Klonen
+  function __clone() {
+    $this->title = "Klon von $this->title";
+    $this->number = self::$anzahl + 1;
+  }
+
+  function __destruct() {
+    echo '<br>Destruktor';
   }
 }
---
+++ php_kurs_woche2/materialien/beispiele_notizmanager/04_oop/class/Tier.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Interface Tier
+ * 
+ * Ein Interface legt fest, welche Methoden eine abgeleitete Klasse zwingend implementieren muss.
+ * Ein Interface legt *nicht* fest was diese Methode ausführen soll
+ */
+interface Tier {
+  public function getRasse();
+}
