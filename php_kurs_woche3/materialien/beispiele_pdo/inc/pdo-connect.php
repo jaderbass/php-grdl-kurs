2fa671ebdce6d44f489cc62e00b50480d5ba5d26
jaderbass <joerg@jaderbass.de>
2025-11-19 12:28:32 +0000

    Datenbank-Verbindung angelegt, erste Beispiele mit SELECT und INSERT INTO

---
+++ php_kurs_woche3/materialien/beispiele_pdo/inc/pdo-connect.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+const DB_USER = 'php_user';
+const DB_PASSWORD = 'qU2+8i2N&vNy_tA-)PRI]Te>';
+const DB_HOST = 'localhost';
+const DB_NAME = 'notizmanager';
+
+try {
+  $pdo = new PDO('mysql:host='. DB_HOST .';dbname=' . DB_NAME . ';charset=utf8mb4',DB_USER , DB_PASSWORD, [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+  ]);
+} catch (PDOException $e) {
+  echo 'DB-Fehler: ' . htmlspecialchars($e->getMessage());
+}
