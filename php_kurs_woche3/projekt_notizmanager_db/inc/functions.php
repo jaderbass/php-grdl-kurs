851f35725c69cf3cc8c1d8496c6818b996be405d
jaderbass <joerg@jaderbass.de>
2025-11-20 13:26:37 +0000

    Funktion deleteNote() hinzugefügt und Löschen-Funktionalität hinzugefügt

--- php_kurs_woche3/projekt_notizmanager_db/inc/functions.php
+++ php_kurs_woche3/projekt_notizmanager_db/inc/functions.php
@@ -31,3 +31,8 @@ function updateNote(PDO $pdo, int $id, string $title, string $content, ?int $cat
   $stmt = $pdo->prepare('UPDATE notes SET title=:t, content=:c, category_id=:cat WHERE id=:id');
   $stmt->execute([':t'=>$title, ':c'=>$content, ':cat'=> $categoryId, ':id'=>$id]);
 }
+
+function deleteNote(PDO $pdo, int $id): void {
+  $stmt = $pdo->prepare('DELETE FROM notes WHERE id=:id');
+  $stmt->execute([':id'=>$id]);
+}
