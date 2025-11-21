4198d6e26a0a736fc4e361cb13d603afa048e3ed
jaderbass <joerg@jaderbass.de>
2025-11-21 08:05:51 +0000

    Navigationsleiste hinzugefügt

--- php_kurs_woche3/projekt_notizmanager_db/public/header.php
+++ php_kurs_woche3/projekt_notizmanager_db/public/header.php
@@ -8,6 +8,8 @@ session_start();
 
 $uri = $_SERVER['SCRIPT_FILENAME'];
 
+$path = ( !str_ends_with(dirname($uri),'public') ) ? '../' : '' ;
+
 require_once __DIR__ . '/../inc/db-connect.php';
 require_once __DIR__ . '/../inc/functions.php';
 ?>
@@ -17,18 +19,14 @@ require_once __DIR__ . '/../inc/functions.php';
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Noziz-Manager DB</title>
-  <?php if( str_ends_with(dirname($uri),'categories') ): ?>
-    <link rel="stylesheet" href="../../css/style.css">
-  <?php else: ?>
-    <link rel="stylesheet" href="../css/style.css">
-  <?php endif; ?>
+  
+  <link rel="stylesheet" href="<?= $path ?>../css/style.css">
 </head>
 <body>
+  <?php include_once 'nav.php' ?>
   <header>
     <div class="container">
       <h1>Notiz-Manager DB</h1>
-      <div class="text-muted">
-        Manage User Login | <a href="categ-manager.php">Kategorie-Manager</a>
-      </div>
+      
     </div>
   </header>
---
+++ php_kurs_woche3/projekt_notizmanager_db/public/nav.php
@@ -0,0 +1,14 @@
+<?php
+  
+?>
+<nav>
+  <div class="container">
+    <ul>
+      <li><a href="<?= $path ?>index.php">Notizen</a></li>
+      <li><a href="<?= $path ?>categ-manager.php">Kategorien</a></li>
+    </ul>
+    <div class="text-muted">
+      Manage User Login
+    </div>
+  </div>
+</nav>
