09f9ead75105580cca7f469a1a88027e2e107cf3
jaderbass <joerg@jaderbass.de>
2025-11-26 13:41:59 +0000

    Beispiele Datei-Upload ausgeführt

--- php_kurs_woche3/datei-upload/phpinfo.php
+++ php_kurs_woche3/datei-upload/phpinfo.php
@@ -1,1 +1,6 @@
-<?php phpinfo(); ?>
+<?php
+/**
+! Diese Datei möglichst schnell vom Server wieder löschen und nur dann gebrauchen, wenn die Informationen nicht anders erreicht werden können, da sie sensible Informationen enhält!
+*/
+ phpinfo(); 
+?>
